0349a665cf67d0c5fc8a29c6178021c0687917b4
Devin <guozwhero@126.com>
2023-10-25 11:58:18 +0800

    666
    666

--- 2. Rust所有权/src/lib.rs
+++ 2. Rust所有权/src/lib.rs
@@ -1,10 +1,21 @@
 
+fn foo(s: String) -> String{
+    println!("{s}");
+    s
+}
+
+#[derive(Debug)]
+struct User {
+    man:bool,
+    age:u64,
+}
 
 /**
  * TODO: 第二课 Rust所有权-课堂笔记及思考题
  */
 #[cfg(test)] // 标注测试模块
 mod tests {
+    use super::*;
 
     #[test]
     fn test0() {
@@ -33,6 +44,68 @@ mod tests {
 
     #[test]
     fn test3() {
-        
+         let s1 = String::from("I am a superman.");
+         let s1 = foo(s1);
+         println!("{s1}");
     }
+
+    fn extend_tup(mut tup : (i32, f64, char, &str)) -> (i32, f64, char, &str) {
+        tup.1 = 3.1415f64;
+        tup.3 = "uuuu";
+        tup
+    } 
+
+    #[test]
+    fn test4() {
+        //TODO: 不可变引用是copy的
+        let tup= (10, 30.2, '9', "xxx");
+        let tup_1 = extend_tup(tup);
+        println!("tup: {:?}", tup);
+        println!("tup_1: {:?}", tup_1);
+
+        let ss = "xxx555果果";
+        let ss1 = ss;
+        println!("ss: {}", ss);
+        println!("ss1: {}", ss1);
+    }
+
+    #[test]
+    fn test5() {
+        let u = User{
+            man:true,
+            age:10
+        };
+        let u1 = u;
+        println!("{:?}", u1);
+        //println!("{:?}", u);  //TODO: 结构体中的变量即使都是copy操作的，如u32，bool 等，但是结构体还是存放在heap中，struct 默认值move操作
+    }
+
+    #[test]
+    fn test6() {
+        let a = 10u32;
+        let b = &a;
+        let c = &&&&&a;
+        let d = &b;
+        let e = b;  //b和e都是对a的引用，由于引用是固定长度的值，做的就引用的复制操作，而并没有对a的值10u32再复制一份
+        println!("{a}");
+        println!("{b}");
+        println!("{c}");
+        println!("{d}");
+        println!("{e}");
+    }
+
+    #[test]
+    fn test7() {
+        let s1 = String::from("I am a superman.");
+        let s2 = &s1;
+        let s3 = &&&&&s1;
+        let s4 = &s2;
+        let s5 = s2;
+        println!("{s1}");
+        println!("{s2}");
+        println!("{s3}");
+        println!("{s4}");
+        println!("{s5}");
+    }
+
 }
