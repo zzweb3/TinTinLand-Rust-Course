d48bdddc90da000a29fc2ad8483184620c0509df
Devin <guozwhero@126.com>
2023-10-26 13:03:41 +0800

    666
    666

--- 3. 复合类型/src/lib.rs
+++ 3. 复合类型/src/lib.rs
@@ -1,4 +1,11 @@
 
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email:String,
+    sign_in_count: u32,
+}
 
 /**
  * TODO: 第三课 复合类型-课堂笔记及思考题
@@ -9,7 +16,31 @@ mod tests {
 
     #[test]
     fn test0() {
-        
+        let mut user1 = User {
+            active: true,
+            username: String::from("someusername123"),
+            email: String::from("someone@example.com"),
+            sign_in_count:1,
+        };
+
+        user1.email = String::from("anotheremail@example.com");
+
+        println!("{:#?}", user1);
+    }
+
+    #[test]
+    fn test1() {
+        let active = true;
+        let username = String::from("someusername123");
+        let email = String::from("someone@example.com");
+        let user1 = User{
+            active,
+            username,
+            email,
+            sign_in_count : 1,
+        };
+
+        println!("{:#?}", user1);
     }
 
 }
